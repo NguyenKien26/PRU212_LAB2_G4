7c9935c99ba3e884c91b9779df4e0e68aec82e87
Tri Nguyen Minh <minhtri10504@gmail.com>
2025-06-20 10:19:10 +0700

    back flip + json score

---
+++ SnowBoarder/Assets/Script/Barrier.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+    [SerializeField] private float jumpHeightThreshold = 0.5f; // Minimum height to consider the skier "jumping"
+    [SerializeField] private string playerTag = "Player"; // Tag of the player object
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the colliding object is the player
+        if (other.CompareTag(playerTag))
+        {
+            // Get the player's position and the barrier's position
+            float playerHeight = other.transform.position.y;
+            float barrierHeight = transform.position.y;
+
+            // Check if the player is high enough to be considered jumping
+            bool isJumping = playerHeight > barrierHeight + jumpHeightThreshold;
+
+            if (!isJumping)
+            {
+                // Player failed to jump over the barrier, trigger game over
+                GameManager.Instance.GameOver(GameManager.Instance.currentScore, GameManager.Instance.currentDistance);
+            }
+            // If the player is jumping, they clear the barrier and no action is needed
+        }
+    }
+}
--- SnowBoarder/Assets/Script/LevelSelector.cs
+++ SnowBoarder/Assets/Script/LevelSelector.cs
@@ -1,19 +1,30 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
 public class LevelSelector : MonoBehaviour
 {
     public int level;
     public TextMeshProUGUI levelText;
+    private Button button;
 
     void Start()
     {
+        button = GetComponent<Button>();
         levelText.text = level.ToString();
+
+        // Kiểm tra trạng thái khóa/mở
+        bool isUnlocked = GameManager.Instance.IsLevelUnlocked(level);
+        button.interactable = isUnlocked;
+        levelText.alpha = isUnlocked ? 1f : 0.5f; // Làm mờ text nếu khóa
     }
 
-    public void OpenScence()
+    public void OpenScene()
     {
-        SceneManager.LoadScene("Level" + level.ToString());
+        if (GameManager.Instance.IsLevelUnlocked(level))
+        {
+            SceneManager.LoadScene("Level" + level.ToString());
+        }
     }
 }
